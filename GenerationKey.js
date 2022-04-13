eafcf9751ca9407526d8862b415cd1d1f8583f5d
ryir <d.petrik@manao-team.com>
2022-04-13 18:24:26 +0300

    Наработка4

--- GenerationKey.js
+++ GenerationKey.js
@@ -1,22 +1,8 @@
 class GenerationKey {
     hmac(message, key) {
-        const utf8 = new TextEncoder().encode(message + key);
-        return crypto.subtle.digest('SHA-256', utf8).then((hashBuffer) => {
-            const hashArray = Array.from(new Uint8Array(hashBuffer));
-            const hashHex = hashArray
-                .map((bytes) => bytes.toString(16).padStart(2, '0'))
-                .join('');
-            return hashHex.toLocaleUpperCase();
-        });
+        return createHash('sha256').update(message + key).digest('hex');
     }
     hmacKey() {
-        const utf8 = new TextEncoder().encode(Math.random(100) + "");
-        return crypto.subtle.digest('SHA-256', utf8).then((hashBuffer) => {
-            const hashArray = Array.from(new Uint8Array(hashBuffer));
-            const hashHex = hashArray
-                .map((bytes) => bytes.toString(16).padStart(2, '0'))
-                .join('');
-            return hashHex.toLocaleUpperCase();
-        });
+        return createHash('sha256').update(Math.random(100) + "").digest('hex');
     }
 }
