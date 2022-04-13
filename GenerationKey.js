69a0ce74b9b697da5f7d86eb2228e3904707394b
ryir <d.petrik@manao-team.com>
2022-04-13 19:18:04 +0300

    Наработка8

--- GenerationKey.js
+++ GenerationKey.js
@@ -1,9 +1,16 @@
 class GenerationKey {
-    crypto = require('crypto');
+    constructor() {
+        let crypto;
+        try {
+            this.crypto = require('crypto');
+        } catch (err) {
+            console.log('crypto support is disabled!');
+        }
+    }
     hmac(message, key) {
-        return crypto.createHash('sha256').update(message + key).digest('hex').toUpperCase();
+        return this.crypto.createHash('sha256').update(message + key).digest('hex').toUpperCase();
     }
     hmacKey() {
-        return crypto.createHash('sha256').update(Math.random(100) + "").digest('hex').toUpperCase();
+        return this.crypto.createHash('sha256').update(Math.random(100) + "").digest('hex').toUpperCase();
     }
 }
